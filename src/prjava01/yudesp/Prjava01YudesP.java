5c548576ee84e40b92dcc38c2cb69fbb20a2b484
ubu64 <ubu64@ubu64DVL-Yudes.F>
2014-05-21 23:37:41 +0200

    Versió 0.1 del projecte prjava01-YudesP

---
+++ src/prjava01/yudesp/Prjava01YudesP.java
@@ -0,0 +1,24 @@
+
+package prjava01.yudesp;
+import java.awt.Desktop;
+import java.io.*;
+
+public class Prjava01YudesP {
+    public static void main(String[] args) throws IOException{
+        File f = new File("prjava01-YudesP.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
+            bw.write("<html>");bw.newLine();
+            bw.write("<head>");bw.newLine();
+            bw.write("<title>");bw.newLine();
+            bw.write("Nova p&agrave;gina web de Paco Yudes");bw.newLine();
+            bw.write("</title>");bw.newLine();
+            bw.write("</head>");bw.newLine();
+            bw.write("<body>");bw.newLine();
+            bw.write("<Nova p&agrave;gina web de Paco Yudes>");bw.newLine();
+            bw.write("</body>");bw.newLine();
+            bw.write("</html>");bw.newLine();
+            bw.close();
+        }
+    }
+    
+}

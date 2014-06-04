39b6351f65659df63d1ab1b89bb0d25dfdd930af
u1w7 <u1w7@192.168.0.241>
2014-06-04 19:47:20 +0100

    Versió 0.2 del projecte prjava01-YudesP

--- src/prjava01/yudesp/Prjava01YudesP.java
+++ src/prjava01/yudesp/Prjava01YudesP.java
@@ -10,7 +10,7 @@ public class Prjava01YudesP {
             bw.write("<html>");bw.newLine();
             bw.write("<head>");bw.newLine();
             bw.write("<title>");bw.newLine();
-            bw.write("Nova p&agrave;gina web de Paco Yudes");bw.newLine();
+            bw.write("DAW2 M4");bw.newLine();
             bw.write("</title>");bw.newLine();
             bw.write("</head>");bw.newLine();
             bw.write("<body>");bw.newLine();
